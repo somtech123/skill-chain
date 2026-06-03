18ba74e23bb9355b326dd158b2421d8fe65a4518
somtech123 <oscardre8@gmail.com>
2026-06-02 23:21:13 -0700

    fix: added production and dev env in shared

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -3,10 +3,21 @@ export const config = {
     BACKEND_URL: "http://localhost:3002",
     FRONTEND_URL: "http://localhost:3000",
   },
-  prod: { BACKEND_URL: "", FRONTEND_URL: "" },
+  prod: {
+    BACKEND_URL: "my-appbackend-production.up.railway.app",
+    FRONTEND_URL: "",
+  },
 };
 
-export const BACKEND_URL = process.env.BACKEND_URL ?? config.dev.BACKEND_URL;
+const isProd = process.env.NODE_ENV === "production";
+
+export const BACKEND_URL =
+  process.env.BACKEND_URL ??
+  (isProd ? config.prod.BACKEND_URL : config.dev.BACKEND_URL);
+
+export const FRONTEND_URL =
+  process.env.FRONTEND_URL ??
+  (isProd ? config.prod.FRONTEND_URL : config.dev.FRONTEND_URL);
 
 export const RPC_URLS: Record<number, string> = {
   11155111: process.env.SEPOLIA_RPC_URL!, // Sepolia
