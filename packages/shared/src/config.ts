9ccc9d037e604e209508545b1540ac943529db3b
somtech123 <oscardre8@gmail.com>
2026-06-02 21:19:56 -0700

    fix: upgrade next.js to 15.3.8

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -3,6 +3,7 @@ export const config = {
     BACKEND_URL: "http://localhost:3002",
     FRONTEND_URL: "http://localhost:3000",
   },
+  prod: { BACKEND_URL: "", FRONTEND_URL: "" },
 };
 
 export const BACKEND_URL = process.env.BACKEND_URL ?? config.dev.BACKEND_URL;
